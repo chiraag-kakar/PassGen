00cd2f565c58890afee55a2518bfe961290bfbdf
Chiraag Kakar <51873854+chiraag-kakar@users.noreply.github.com>
2020-08-13 20:00:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,131 @@
+//Dom elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
+
+//Genereate Event listen
+generate.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(
+        hasLower, 
+        hasUpper, 
+        hasNumber, 
+        hasSymbol,
+        length
+    );
+});
+
+// Copy to clipboard
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if(!password) {
+        return
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password copied to clipboard!');
+})
+
+//Generate password function 
+function generatePassword(lower, upper, number, symbol, length){
+    // 1. Init pw var
+    // 2. filter out unchecked types
+    // 3. Loop over length all generater function for each type
+    // 4. Add final pw to the pw var and return
+
+    let generatedPassword = '';
+
+    const typesCount = lower + upper + number + symbol;
+
+    // console.log('typesCount:', typesCount);
+
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(
+        item => Object.values(item)[0]
+    );
+
+    // console.log('typesArr:', typesArr);
+
+    if(typesCount === 0){
+        return '';
+    }
+
+    for(let i = 0; i < length; i += typesCount){
+        typesArr.forEach(type =>{
+            const funcName = Object.keys(type)[0];
+
+            // console.log('funcName:', funcName);
+
+            generatedPassword += randomFunc[funcName]();
+        });
+    }
+    const finalPassword = generatedPassword.slice(0, length);
+    return finalPassword;
+}
+
+//Genertor function http://www.net-comber.com/charset.html
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>/,.';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible');
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible');
+});
+
+
+// Live Count
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/hit/chiraag-kakar.github.io/passgen/visits')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
+}
