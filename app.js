910a2d209c62f915f5d1f8db3ed2ca2ddd8fdd85
Chiraag Kakar <51873854+chiraag-kakar@users.noreply.github.com>
2020-08-13 23:34:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -129,3 +129,17 @@ function updateVisitCount() {
 		countEl.innerHTML = res.value;
 	})
 }
+
+//Dark Theme
+const toggle = document.getElementById('toggle');
+const body = document.body;
+
+toggle.addEventListener('input', (e) => {
+	const isChecked = e.target.checked;
+	
+	if(isChecked) {
+		body.classList.add('dark-theme');
+	} else {
+		body.classList.remove('dark-theme');
+	}
+});
